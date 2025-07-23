2283ee75b0cdc94c7d183c90539b16afb162aa73
pratiksingh018 <pratiksingh1288@gmail.com>
2025-07-23 14:29:59 +0000

    1.variable

---
+++ 1_variables/variables.js
@@ -0,0 +1,25 @@
+// Variable - A container that stores a values.
+
+// let name = "Code world";
+// let age = 20;
+// let student = true;
+
+// console.log(`My name is ${name}`);
+// console.log(`I am ${age} years old`);
+// console.log(typeof name);  // string data type.
+// console.log(typeof age); // number data type.
+// console.log(typeof student);  //boolean data type.
+
+let firstName = 'Code world';
+let age = 21;
+let student = true;
+
+document.getElementById('x').textContent = firstName;
+document.getElementById('y').textContent = age;
+document.getElementById('z').textContent = student;
+
+
+
+
+
+
