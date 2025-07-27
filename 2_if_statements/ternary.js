eb2637cc7abb33dac1d5b3c03ddc4210e0af3da0
pratiksingh018 <pratiksingh1288@gmail.com>
2025-07-27 11:13:29 +0000

    ternary_operator

---
+++ 2_if_statements/ternary.js
@@ -0,0 +1,16 @@
+// ternary operator = a shortcut to if{} else{} statement.
+//                    help to assign a variable based on a condition
+//                    condition ? code : code;
+
+// let age = 12;
+// let input = age >= 18 ? "You are an adult": "You are not an adult";
+// console.log(input);
+
+// let isStudent = false
+// let inp = isStudent ? "You are a student" : "You are not a student";
+// console.log(inp);
+
+let amount = 4666;
+let discount = amount >= 100? 10 : 0;
+console.log(`Your total is ${amount + amount * (discount)}`);
+
