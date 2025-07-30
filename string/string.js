6ee77647eae2d2059c88b0ca076e9b5b900a4e73
pratiksingh018 <pratiksingh1288@gmail.com>
2025-07-30 06:34:45 +0000

    string and slicing

---
+++ string/string.js
@@ -0,0 +1,30 @@
+// string method = The string method is allow you to manipulate and work with text.
+
+// let Name = "CodeBaba";
+
+// console.log(Name.charAt(1));
+// console.log(Name.indexOf('d'));
+// console.log(Name.lastIndexOf('e'));
+// console.log(Name.length);
+// console.log(Name.toUpperCase());
+// console.log(Name.toLowerCase());
+// console.log(Name.replaceAll('a','o'));
+
+
+// string slicing = creating a substring from a portion of another string .
+
+// string.slice(start, end);
+
+const myName = 'code baba';
+
+let firstNam = myName.slice(0, 2);
+let lastnum = myName.slice(2, 6);
+
+console.log(firstNam);
+console.log(lastnum);
+
+
+
+
+
+
